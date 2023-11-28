77673b4ca4f7eb342cf84310e5ca995486dbac61
oxyhome6688 <oxyhome6688@gmail.com>
2023-11-28 15:05:56 +0700

    add more log

--- VinPlayUserCore/src/main/java/com/vinplay/dichvuthe/dao/impl/RechargeDaoImpl.java
+++ VinPlayUserCore/src/main/java/com/vinplay/dichvuthe/dao/impl/RechargeDaoImpl.java
@@ -2891,6 +2891,7 @@ public class RechargeDaoImpl
             Document conditions = new Document();
             conditions.put("Id", (Object) Id);
             Document document = (Document) db.getCollection(DvtConst.DEPOSIT_BANK_COLLECTION).find((Bson) conditions).first();
+            logger.info("document " + document + " id: " + Id);
             if (document == null)
                 return null;
             DepositBankModel model = new DepositBankModel(
