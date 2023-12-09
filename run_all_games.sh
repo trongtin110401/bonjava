d283c36ea21f543b188367856c8c425b3144bb88
abc <dontsailam1997@gmail.com>
2023-12-09 11:18:02 +0700

    add run_all

--- run_all_games.sh
+++ run_all_games.sh
@@ -15,34 +15,26 @@ killProcess() {
 # run vbee
 runVbee() {
     currentDir="api/vbee"
-    cd $currentDir
-    ../../gradlew clean
-    ../../gradlew build
+
     cd $SCRIPTPATH;
     nohup java -cp "libs/*:api/vbee/libs/*:api/vbee/build/libs/vbee-1.0.jar" com.vinplay.vbee.main.VBeeMain >/dev/null 2>&1 &
 }
 runBackend() {
     currentDir="api/VinPlayBackend"
-    cd $currentDir
-    ../../gradlew clean
-    ../../gradlew build
+
     cd $SCRIPTPATH;
     nohup java -cp "libs/*:api/VinPlayBackend/libs/*:api/VinPlayBackend/build/libs/VinPlayBackend-1.0.jar" com.vinplay.api.backend.server.VinPlayBackendMain >/dev/null 2>&1 &
 
 }
 runPortal(){
   currentDir="api/VinPlayPortal"
-    cd $currentDir
-    ../../gradlew clean
-    ../../gradlew build
+
     cd $SCRIPTPATH;
     nohup java -cp "libs/*:api/VinPlayPortal/libs/*:api/VinPlayPortal/build/libs/VinPlayPortal.jar" com.vinplay.api.server.JettyServer >/dev/null 2>&1 &
 }
 runWspay(){
     currentDir="api/wspay"
-    cd $currentDir
-    ../../gradlew clean
-    ../../gradlew build
+
     cd $SCRIPTPATH;
     nohup java -cp "libs/*:api/wspay/libs/*:api/wspay/build/libs/wspay.jar" com.vinplay.pay.server.JettyServer >/dev/null 2>&1 &
 
@@ -50,9 +42,7 @@ runWspay(){
 
 runWsReport(){
     currentDir="api/wsreport"
-    cd $currentDir
-    ../../gradlew clean
-    ../../gradlew build
+
     cd $SCRIPTPATH;
     nohup java -cp "libs/*:${currentDir}/libs/*:${currentDir}/build/libs/wsreport-1.0-SNAPSHOT.jar" game.GameApplication >/dev/null 2>&1 &
 
@@ -61,9 +51,7 @@ runWsReport(){
 
 runMiniGame(){
     currentDir="game/Minigame"
-    cd $currentDir
-    ../../gradlew clean
-    ../../gradlew build
+
     cd $SCRIPTPATH;
     nohup java -cp "libs/*:game/Minigame/libs/*:game/Minigame/build/libs/Minigame.jar" game.MiniGameMain 1>logoutmini.txt &
 
@@ -72,9 +60,7 @@ runMiniGame(){
 
 runSlot(){
     currentDir="game/slot"
-    cd $currentDir
-    ../../gradlew clean
-    ../../gradlew build
+
     cd $SCRIPTPATH;
     nohup java -cp "libs/*:${currentDir}/libs/*:${currentDir}/build/libs/SlotMachine.jar" game.SlotMain >/dev/null 2>&1 &
 
@@ -83,9 +69,7 @@ runSlot(){
 ### run bacay ###
 runBacay(){
     currentDir="game/bacayServer"
-    cd $currentDir
-    ../../gradlew clean
-    ../../gradlew build
+
     cd $SCRIPTPATH;
     nohup java -cp "libs/*:${currentDir}/libs/*:${currentDir}/build/libs/bacayServer.jar" game.bacay.server.BacayMain >/dev/null 2>&1 &
 }
@@ -93,9 +77,7 @@ runBacay(){
 ### run binh ###
 runBinh(){
     currentDir="game/binh"
-    cd $currentDir
-    ../../gradlew clean
-    ../../gradlew build
+
     cd $SCRIPTPATH;
     nohup java -cp "libs/*:${currentDir}/libs/*:${currentDir}/build/libs/binh.jar" game.binh.server.BinhMain >/dev/null 2>&1 &
 }
@@ -103,9 +85,7 @@ runBinh(){
 ### run tlmn ###
 runTienLen(){
     currentDir="game/tlmn"
-    cd $currentDir
-    ../../gradlew clean
-    ../../gradlew build
+
     cd $SCRIPTPATH;
     nohup java -cp "libs/*:${currentDir}/libs/*:${currentDir}/build/libs/tlmn.jar" game.tienlen.server.TlmnMain >/dev/null 2>&1 &
 }
@@ -113,9 +93,7 @@ runTienLen(){
 # run Poker
 runPoker() {
     currentDir="game/poker"
-    cd $currentDir
-    ../../gradlew clean
-    ../../gradlew build
+
     cd $SCRIPTPATH;
     nohup java -cp "libs/*:${currentDir}/libs/*:${currentDir}/build/libs/poker.jar" game.poker.server.PokerMain >/dev/null 2>&1 &
 }
@@ -123,18 +101,14 @@ runPoker() {
 # run baucuato2
 runBauCuaTo2() {
     currentDir="game/baucuato2"
-    cd $currentDir
-    ../../gradlew clean
-    ../../gradlew build
+
     cd $SCRIPTPATH;
     nohup java -cp "libs/*:${currentDir}/libs/*:${currentDir}/build/libs/baucuato2.jar" game.BauCuaTo2Main >/dev/null 2>&1 &
 }
 
 runTaiXiuMini() {
     currentDir="game/taixiuMini"
-    cd $currentDir
-    ../../gradlew clean
-    ../../gradlew build
+
     cd $SCRIPTPATH;
     nohup java -cp "libs/*:${currentDir}/libs/*:${currentDir}/build/libs/taixiuMini.jar" game.TaiXiuMiniGameMain >/dev/null 2>&1 &
 }
@@ -142,9 +116,7 @@ runTaiXiuMini() {
 ### run Xi Dach ###
 runXiDach(){
     currentDir="game/xizach"
-    cd $currentDir
-    ../../gradlew clean
-    ../../gradlew build
+
     cd $SCRIPTPATH;
     nohup java -cp "libs/*:${currentDir}/libs/*:${currentDir}/build/libs/xizach.jar" game.xizach.server.XiZachMain >/dev/null 2>&1 &
 
@@ -166,9 +138,7 @@ runXiDach(){
 runXocDia(){
   echo "Start building Xoc Dia..."
     currentDir="game/xocdia"
-    cd $currentDir
-    ../../gradlew clean
-    ../../gradlew build
+
     cd $SCRIPTPATH;
     nohup java -cp "libs/*:${currentDir}/libs/*:${currentDir}/build/libs/xocdia.jar" game.xocdia.server.XocDiaMain >/dev/null 2>&1 &
    #java -cp "libs/*:${currentDir}/libs/*:${currentDir}/build/libs/baicao.jar" game.baicao.server.BaiCaoMain
@@ -179,9 +149,7 @@ runXocDia(){
 runSam(){
   echo "Start building Sam..."
     currentDir="game/sam"
-    cd $currentDir
-    ../../gradlew clean
-    ../../gradlew build
+
     cd $SCRIPTPATH;
     nohup java -cp "libs/*:${currentDir}/libs/*:${currentDir}/build/libs/sam.jar" game.sam.server.SamMain >/dev/null 2>&1 &
    #java -cp "libs/*:${currentDir}/libs/*:${currentDir}/build/libs/sam.jar" game.sam.server.SamMain
