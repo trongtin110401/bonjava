ccc5502d0b48d1de66c4ad5b93d46634ac277dc0
oxy <oxyhome6868@gmail.com>
2023-12-25 23:53:59 +0700

    improve ObjectMapper

--- api/wsreport/src/main/java/game/scheduler/ScheduledTasks.java
+++ api/wsreport/src/main/java/game/scheduler/ScheduledTasks.java
@@ -241,7 +241,7 @@ public class ScheduledTasks { // chay schedule lien tuc // cach nay chi dung cho
             cacheService.removeKey(USER_TAI_XIU_MD5);
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println("Loi senTXAdmin");
+            System.out.println("Loi senTXMD5Admin");
 
         }
     }
