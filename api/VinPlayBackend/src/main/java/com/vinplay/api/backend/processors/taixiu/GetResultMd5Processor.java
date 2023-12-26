3550899e665900e2f4813988349ab29445c21f58
oxyhome6688 <oxyhome6688@gmail.com>
2023-12-26 20:52:31 +0700

    add api get md5 hash

---
+++ api/VinPlayBackend/src/main/java/com/vinplay/api/backend/processors/taixiu/GetResultMd5Processor.java
@@ -0,0 +1,51 @@
+package com.vinplay.api.backend.processors.taixiu;
+
+import com.vinplay.dal.service.impl.TaiXiuMd5ServiceImpl;
+import com.vinplay.vbee.common.cp.BaseProcessor;
+import com.vinplay.vbee.common.cp.Param;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+
+public class GetResultMd5Processor implements BaseProcessor<HttpServletRequest, String> {
+    @Override
+    public String execute(Param<HttpServletRequest> param) throws SQLException {
+        HttpServletRequest request = param.get();
+        String plainText = request.getParameter("plainText");
+        String response;
+        TaiXiuMd5ServiceImpl taiXiuMd5Service = new TaiXiuMd5ServiceImpl();
+        response = taiXiuMd5Service.getHashMd5(plainText);
+        if (response == null) {
+            response = hashMD5(plainText);
+        }
+        return response;
+    }
+
+
+    public static String hashMD5(String input) {
+        try {
+            // Create an MD5 message digest
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            // Update the message digest with the input bytes
+            md.update(input.getBytes());
+
+            // Get the hash value as an array of bytes
+            byte[] digest = md.digest();
+
+            // Convert the byte array to a hexadecimal string
+            StringBuilder result = new StringBuilder();
+            for (byte b : digest) {
+                result.append(String.format("%02x", b));
+            }
+
+            return result.toString();
+        } catch (NoSuchAlgorithmException e) {
+            // Handle the exception, e.g., log it or throw a custom exception
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
