d54f52b5e4608868d1ffdc3963c216af69e69163
TU TRINH <tu.trinh@weeds.co.kr>
2024-01-05 17:46:54 +0700

    leaderboard service

---
+++ api/BoardService/src/main/java/game/controller/LeaderboardController.java
@@ -0,0 +1,41 @@
+package game.controller;
+
+import game.dto.data.UserScore;
+import game.dto.request.LeaderboardCriteria;
+import game.dto.request.LeaderboardParam;
+import game.dto.response.Response;
+import game.service.LeaderboardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/leaderboard")
+public class LeaderboardController {
+
+    @Autowired
+    private LeaderboardService leaderboardService;
+
+    @GetMapping
+    public ResponseEntity receive(@ModelAttribute LeaderboardCriteria criteria) {
+        List<UserScore> results = leaderboardService.receiveData(criteria);
+        return new ResponseEntity(Response.builder()
+                .message("success")
+                .status(0)
+                .data(results)
+                .build(), HttpStatus.OK);
+    }
+
+
+    @PostMapping
+    public ResponseEntity add(@RequestBody LeaderboardParam param) {
+        leaderboardService.addScore(param);
+        return new ResponseEntity(Response.builder()
+                .message("success")
+                .status(0)
+                .build(), HttpStatus.OK);
+    }
+}
---
+++ api/BoardService/src/main/java/game/service/impl/LeaderboardServiceImpl.java
@@ -0,0 +1,37 @@
+package game.service.impl;
+
+import game.dto.data.UserScore;
+import game.dto.request.LeaderboardCriteria;
+import game.dto.request.LeaderboardParam;
+import game.repository.LeaderboardRepository;
+import game.service.LeaderboardService;
+import org.redisson.client.protocol.ScoredEntry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class LeaderboardServiceImpl implements LeaderboardService {
+
+    @Autowired
+    private LeaderboardRepository leaderboardRepository;
+
+    @Override
+    public void addScore(LeaderboardParam param) {
+        leaderboardRepository.addScore(param.getBoardName(), param.getUsername(), param.getScore());
+    }
+
+    @Override
+    public List<UserScore> receiveData(LeaderboardCriteria criteria) {
+        int fromIndex = (criteria.getPageIndex() - 1) * criteria.getPageSize();
+        int endIndex = fromIndex + criteria.getPageSize();
+        Collection<ScoredEntry<String>> scoredEntries =
+                leaderboardRepository.receiveData(criteria.getBoardName(), fromIndex, endIndex);
+        return scoredEntries.stream()
+                .map(entry -> new UserScore(entry.getValue(), entry.getScore().longValue()))
+                .collect(Collectors.toList());
+    }
+}
