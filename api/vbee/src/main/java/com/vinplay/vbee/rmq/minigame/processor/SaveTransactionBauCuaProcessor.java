e80c137b9c61ee26003905db07f297c5be7d423f
oxyhome6688 <oxyhome6688@gmail.com>
2024-01-13 17:55:56 +0700

    update vinh danh baucua

--- api/vbee/src/main/java/com/vinplay/vbee/rmq/minigame/processor/SaveTransactionBauCuaProcessor.java
+++ api/vbee/src/main/java/com/vinplay/vbee/rmq/minigame/processor/SaveTransactionBauCuaProcessor.java
@@ -1,6 +1,6 @@
 /*
  * Decompiled with CFR 0.144.
- * 
+ *
  * Could not load the following classes:
  *  com.vinplay.vbee.common.cp.BaseProcessor
  *  com.vinplay.vbee.common.cp.Param
@@ -11,19 +11,37 @@ package com.vinplay.vbee.rmq.minigame.processor;
 
 import com.vinplay.vbee.common.cp.BaseProcessor;
 import com.vinplay.vbee.common.cp.Param;
+import com.vinplay.vbee.common.enums.Games;
 import com.vinplay.vbee.common.messages.BaseMessage;
+import com.vinplay.vbee.common.messages.minigame.TransactionTaiXiuMessage;
 import com.vinplay.vbee.common.messages.minigame.baucua.TransactionBauCuaMsg;
 import com.vinplay.vbee.dao.impl.BauCuaDaoImpl;
+import com.vinplay.vbee.dto.TopVinhDanhDto;
+import com.vinplay.vbee.rmq.report.processor.TopVinhDanhProcessor;
 
 public class SaveTransactionBauCuaProcessor
-implements BaseProcessor<byte[], Boolean> {
+        implements BaseProcessor<byte[], Boolean> {
     public Boolean execute(Param<byte[]> param) {
-        byte[] body = (byte[])param.get();
-        TransactionBauCuaMsg message = (TransactionBauCuaMsg)BaseMessage.fromBytes((byte[])body);
+        byte[] body = param.get();
+        TransactionBauCuaMsg message = (TransactionBauCuaMsg) BaseMessage.fromBytes((byte[]) body);
+        if (message.moneyType == 1) {
+            addTopVinhDanh(message);
+        }
         BauCuaDaoImpl dao = new BauCuaDaoImpl();
-        boolean success = true;
         dao.saveTransactionBauCua(message);
         return true;
+    }
+
+    public void addTopVinhDanh(TransactionBauCuaMsg message) {
+        try {
+            TopVinhDanhDto topVinhDanhDto = new TopVinhDanhDto();
+            topVinhDanhDto.setUsername(message.username);
+            topVinhDanhDto.setScore(message.totalExchange);
+            topVinhDanhDto.setBoardName(Games.BAU_CUA.getName());
+            TopVinhDanhProcessor.addTopVinhDanh(topVinhDanhDto);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
