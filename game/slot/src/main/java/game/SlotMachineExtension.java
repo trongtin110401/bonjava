36a912f9036718c3d42802ff9fc27dc4740b3dcb
oxy <oxyhome6868@gmail.com>
2024-01-20 01:48:18 +0700

    SLOT20

--- game/slot/src/main/java/game/SlotMachineExtension.java
+++ game/slot/src/main/java/game/SlotMachineExtension.java
@@ -54,8 +54,8 @@ public class SlotMachineExtension
         this.addRequestHandler((short) 4000, BentleyModuleExt.class);
         this.addRequestHandler((short) 5000, RollRoyModule.class);
         this.addRequestHandler((short) 12000, SpartanModule.class);
-        this.addRequestHandler((short) 13000, RangeRoverModule.class);
-//        this.addRequestHandler((short) 13000, AuditionModuleExt.class);
+//        this.addRequestHandler((short) 13000, RangeRoverModule.class);
+        this.addRequestHandler((short) 13000, AuditionModuleExt.class);
         this.addRequestHandler((short) 14000, TamHungModule.class);
 
 
