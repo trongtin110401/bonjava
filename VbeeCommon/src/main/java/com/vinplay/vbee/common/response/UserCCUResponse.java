6884129c95ea4964e7fa0f10eb97124c01a13481
oxyhome6688 <oxyhome6688@gmail.com>
2024-02-07 01:41:21 +0700

    update save transaction

---
+++ VbeeCommon/src/main/java/com/vinplay/vbee/common/response/UserCCUResponse.java
@@ -0,0 +1,35 @@
+package com.vinplay.vbee.common.response;
+
+public class UserCCUResponse {
+
+    private String nickName;
+
+    private String gameName;
+
+    private long lastLoginTime;
+
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+
+    public long getLastLoginTime() {
+        return lastLoginTime;
+    }
+
+    public void setLastLoginTime(long lastLoginTime) {
+        this.lastLoginTime = lastLoginTime;
+    }
+}
