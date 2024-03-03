f32db366b2d45c5517ced4c15342ab4a5ca7281d
oxyhome6688 <oxyhome6688@gmail.com>
2024-03-03 15:09:37 +0700

    update get money user TX

---
+++ api/wsreport/src/main/java/game/config/HttpCommon.java
@@ -0,0 +1,24 @@
+package game.config;
+
+import okhttp3.OkHttpClient;
+
+public class HttpCommon {
+    public OkHttpClient httpClient;
+
+    public static HttpCommon _instance;
+
+    public static HttpCommon getInstance(){
+        if(_instance==null){
+            _instance = new HttpCommon();
+        }
+        return _instance;
+    }
+
+    public HttpCommon() {
+        httpClient = new OkHttpClient();
+    }
+
+    public  OkHttpClient getHttpClient() {
+        return httpClient;
+    }
+}
