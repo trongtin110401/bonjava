6a3e3df16076211afbdd1ea912bb02589d236c05
oxy <oxyhome6868@gmail.com>
2024-03-03 10:07:07 +0700

    slot20 extend v2

--- game/slot/src/main/java/game/modules/slot/room/Slot20Room.java
+++ game/slot/src/main/java/game/modules/slot/room/Slot20Room.java
@@ -255,7 +255,7 @@ public class Slot20Room extends SlotRoom {
                                     // phần thưởng cho JACKPOT
                                     else if (award == Slot20Award.PENTA_JACKPOT) {
                                         // đảm bảo chỉ duy nhất 1 dòng trúng JACKPOT
-                                        // nếu trùng lặp, đầu lại dòng vòng lặp while (continue block4)
+                                        // nếu trùng lặp, bắt đầu lại dòng vòng lặp while (continue block4)
                                         for (AwardsOnLine e : awardsOnLines) {
                                             if (e.getAward() != Slot20Award.PENTA_JACKPOT) {
                                                 continue;
