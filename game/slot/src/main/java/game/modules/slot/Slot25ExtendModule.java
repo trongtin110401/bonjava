addbe2f33dfee19f214a8be72bfe8c1805f75d83
oxy <oxyhome6868@gmail.com>
2024-03-03 13:04:59 +0700

    slot25extend line

--- game/slot/src/main/java/game/modules/slot/Slot25ExtendModule.java
+++ game/slot/src/main/java/game/modules/slot/Slot25ExtendModule.java
@@ -83,12 +83,12 @@ public abstract class Slot25ExtendModule extends SlotModule {
 //        this.rooms.put(this.gameName + "_vin_10000",
 //                new Slot25ExtendRoom(this, this.commandCollection, slotLogListener, this.gameName, (byte) 2, this.gameName + "_vin_10000", (short) 1, this.jackpots[2], funds[2], 10000, initJackpotValues[2]));
 
+        this.rooms.put(this.gameName + "_vin_100",
+                new Slot25ExtendRoom(this, this.commandCollection, slotLogListener, this.gameName, (byte) 0, this.gameName + "_vin_100", (short) 1, this.jackpots[0], 10000000000L, 100, initJackpotValues[0]));
         this.rooms.put(this.gameName + "_vin_1000",
-                new Slot25ExtendRoom(this, this.commandCollection, slotLogListener, this.gameName, (byte) 0, this.gameName + "_vin_100", (short) 1, this.jackpots[0], 10000000000L, 1000, initJackpotValues[0]));
+                new Slot25ExtendRoom(this, this.commandCollection, slotLogListener, this.gameName, (byte) 1, this.gameName + "_vin_1000", (short) 1, this.jackpots[1], 10000000000L, 1000, initJackpotValues[1]));
         this.rooms.put(this.gameName + "_vin_10000",
-                new Slot25ExtendRoom(this, this.commandCollection, slotLogListener, this.gameName, (byte) 1, this.gameName + "_vin_1000", (short) 1, this.jackpots[1], 10000000000L, 10000, initJackpotValues[1]));
-        this.rooms.put(this.gameName + "_vin_100000",
-                new Slot25ExtendRoom(this, this.commandCollection, slotLogListener, this.gameName, (byte) 2, this.gameName + "_vin_10000", (short) 1, this.jackpots[2], 10000000000L, 100000, initJackpotValues[2]));
+                new Slot25ExtendRoom(this, this.commandCollection, slotLogListener, this.gameName, (byte) 2, this.gameName + "_vin_10000", (short) 1, this.jackpots[2], 10000000000L, 10000, initJackpotValues[2]));
 
         Debug.trace("INIT " + this.gameName + " DONE");
 
@@ -209,10 +209,8 @@ public abstract class Slot25ExtendModule extends SlotModule {
         } else {
             Debug.trace(this.gameName + ": change room error, leaved= " + cmd.roomLeavedId + ", joined= " + cmd.roomJoinedId);
         }
-
-        if (roomJoined != null) {
-            BroadCastUserState.pushBroadCast(user.getName(), user.getName() + " play " + gameName + " " + roomJoined.getBetValue());
-        }
+        assert roomJoined != null;
+        BroadCastUserState.pushBroadCast(user.getName(), user.getName() + " play " + gameName + " " + roomJoined.getBetValue());
     }
 
     protected void play(User user, DataCmd dataCmd) {
