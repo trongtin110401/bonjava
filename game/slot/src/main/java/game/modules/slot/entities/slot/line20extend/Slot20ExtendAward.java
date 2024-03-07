68588f54d4a66658544ee60096872b652b67189d
oxy <oxyhome6868@gmail.com>
2024-03-07 21:28:24 +0700

    update slot20extend item

--- game/slot/src/main/java/game/modules/slot/entities/slot/line20extend/Slot20ExtendAward.java
+++ game/slot/src/main/java/game/modules/slot/entities/slot/line20extend/Slot20ExtendAward.java
@@ -20,25 +20,41 @@ public enum Slot20ExtendAward {
     TRIPLE_F("TRIPLE_F", 19, (byte) 20, Slot20ExtendItem.F, (byte) 3, 40.0f),
     DOUBLE_F("DOUBLE_F", 19, (byte) 20, Slot20ExtendItem.F, (byte) 2, 2.0f),
 
-    PENTA_E("PENTA_E", 20, (byte) 21, Slot20ExtendItem.E, (byte) 5, 400.0f),
-    QUADAR_E("QUADAR_E", 21, (byte) 22, Slot20ExtendItem.E, (byte) 4, 100.0f),
-    TRIPLE_E("TRIPLE_E", 22, (byte) 23, Slot20ExtendItem.E, (byte) 3, 30.0f),
+    PENTA_E2("PENTA_E2", 20, (byte) 21, Slot20ExtendItem.E2, (byte) 5, 400.0f),
+    QUADAR_E2("QUADAR_E2", 21, (byte) 22, Slot20ExtendItem.E2, (byte) 4, 100.0f),
+    TRIPLE_E2("TRIPLE_E2", 22, (byte) 23, Slot20ExtendItem.E2, (byte) 3, 30.0f),
 
-    PENTA_D("PENTA_D", 23, (byte) 24, Slot20ExtendItem.D, (byte) 5, 250.0f),
-    QUADAR_D("QUADAR_D", 24, (byte) 25, Slot20ExtendItem.D, (byte) 4, 100.0f),
-    TRIPLE_D("TRIPLE_D", 25, (byte) 26, Slot20ExtendItem.D, (byte) 3, 20.0f),
+    PENTA_E1("PENTA_E1", 20, (byte) 21, Slot20ExtendItem.E1, (byte) 5, 400.0f),
+    QUADAR_E1("QUADAR_E1", 21, (byte) 22, Slot20ExtendItem.E1, (byte) 4, 100.0f),
+    TRIPLE_E1("TRIPLE_E1", 22, (byte) 23, Slot20ExtendItem.E1, (byte) 3, 30.0f),
+
+    PENTA_D2("PENTA_D2", 23, (byte) 24, Slot20ExtendItem.D2, (byte) 5, 250.0f),
+    QUADAR_D2("QUADAR_D2", 24, (byte) 25, Slot20ExtendItem.D2, (byte) 4, 100.0f),
+    TRIPLE_D2("TRIPLE_D2", 25, (byte) 26, Slot20ExtendItem.D2, (byte) 3, 20.0f),
+
+    PENTA_D1("PENTA_D1", 23, (byte) 24, Slot20ExtendItem.D1, (byte) 5, 250.0f),
+    QUADAR_D1("QUADAR_D1", 24, (byte) 25, Slot20ExtendItem.D1, (byte) 4, 100.0f),
+    TRIPLE_D1("TRIPLE_D1", 25, (byte) 26, Slot20ExtendItem.D1, (byte) 3, 20.0f),
 
     PENTA_C("PENTA_C", 26, (byte) 27, Slot20ExtendItem.C, (byte) 5, 200.0f),
     QUADAR_C("QUADAR_C", 27, (byte) 28, Slot20ExtendItem.C, (byte) 4, 35.0f),
     TRIPLE_C("TRIPLE_C", 28, (byte) 29, Slot20ExtendItem.C, (byte) 3, 10.0f),
 
-    PENTA_B("PENTA_B", 29, (byte) 30, Slot20ExtendItem.B, (byte) 5, 150.0f),
-    QUADAR_B("QUADAR_B", 30, (byte) 31, Slot20ExtendItem.B, (byte) 4, 30.0f),
-    TRIPLE_B("TRIPLE_B", 31, (byte) 32, Slot20ExtendItem.B, (byte) 3, 5.0f),
+    PENTA_B2("PENTA_B2", 29, (byte) 30, Slot20ExtendItem.B2, (byte) 5, 150.0f),
+    QUADAR_B2("QUADAR_B2", 30, (byte) 31, Slot20ExtendItem.B2, (byte) 4, 30.0f),
+    TRIPLE_B2("TRIPLE_B2", 31, (byte) 32, Slot20ExtendItem.B2, (byte) 3, 5.0f),
 
-    PENTA_A("PENTA_A", 32, (byte) 33, Slot20ExtendItem.A, (byte) 5, 50.0f),
-    QUADAR_A("QUADAR_A", 33, (byte) 34, Slot20ExtendItem.A, (byte) 4, 20.0f),
-    TRIPLE_A("TRIPLE_A", 34, (byte) 35, Slot20ExtendItem.A, (byte) 3, 5.0f);
+    PENTA_B1("PENTA_B1", 29, (byte) 30, Slot20ExtendItem.B1, (byte) 5, 150.0f),
+    QUADAR_B1("QUADAR_B1", 30, (byte) 31, Slot20ExtendItem.B1, (byte) 4, 30.0f),
+    TRIPLE_B1("TRIPLE_B1", 31, (byte) 32, Slot20ExtendItem.B1, (byte) 3, 5.0f),
+
+    PENTA_A2("PENTA_A", 32, (byte) 33, Slot20ExtendItem.A2, (byte) 5, 50.0f),
+    QUADAR_A2("QUADAR_A", 33, (byte) 34, Slot20ExtendItem.A2, (byte) 4, 20.0f),
+    TRIPLE_A2("TRIPLE_A", 34, (byte) 35, Slot20ExtendItem.A2, (byte) 3, 5.0f),
+
+    PENTA_A1("PENTA_A1", 32, (byte) 33, Slot20ExtendItem.A1, (byte) 5, 50.0f),
+    QUADAR_A1("QUADAR_A1", 33, (byte) 34, Slot20ExtendItem.A1, (byte) 4, 20.0f),
+    TRIPLE_A1("TRIPLE_A1", 34, (byte) 35, Slot20ExtendItem.A1, (byte) 3, 5.0f);
 
     private byte id;
     private Slot20ExtendItem item;
--- game/slot/src/main/java/game/modules/slot/entities/slot/line20extend/Slot20ExtendItem.java
+++ game/slot/src/main/java/game/modules/slot/entities/slot/line20extend/Slot20ExtendItem.java
@@ -14,11 +14,15 @@ public enum Slot20ExtendItem {
     WILD("WILD", 3, "Wild", (byte) 2),
     JACKPOT("JACKPOT", 4, "Jackpot", (byte) 3),
     F("F", 6, "F", (byte) 4),
-    E("E", 7, "E", (byte) 5),
-    D("D", 8, "D", (byte) 6),
-    C("C", 9, "C", (byte) 7),
-    B("B", 10, "B", (byte) 8),
-    A("A", 11, "A", (byte) 9);
+    E1("E2", 7, "E2", (byte) 5),
+    E2("E1", 7, "E1", (byte) 6),
+    D1("D2", 8, "D2", (byte) 7),
+    D2("D1", 8, "D1", (byte) 8),
+    C("C", 9, "C", (byte) 9),
+    B1("B2", 10, "B2", (byte) 10),
+    B2("B1", 10, "B1", (byte) 11),
+    A2("A2", 11, "A2", (byte) 12),
+    A1("A1", 11, "A1", (byte) 13);
 
     private String name;
     private byte id;
