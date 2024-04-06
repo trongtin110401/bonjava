473eed606d002edb724279c0452bbe11234150ed
oxyhome6688 <oxyhome6688@gmail.com>
2024-04-06 10:48:38 +0700

    update cash out

--- VinPlayUserCore/src/main/java/com/vinplay/payment/entities/UserWithdraw.java
+++ VinPlayUserCore/src/main/java/com/vinplay/payment/entities/UserWithdraw.java
@@ -19,7 +19,6 @@ public class UserWithdraw {
     public String Status;
     public int Version;
     public String UserProve;
-    private String type;
 
     public UserWithdraw(String id, String username, int amount, String bankAccountNumber, String bankAccountName, String bankName, String createdAt, String updatedAt, String status, int version) {
         Id = id;
@@ -84,14 +83,6 @@ public class UserWithdraw {
         Status = status;
         Version = version;
         UserProve = userProve;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
     }
 
     public String toJson() {
