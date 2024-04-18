4d2d05ae84432ef1c398a3503221b301136ce0e1
oxyhome6688 <oxyhome6688@gmail.com>
2024-04-18 12:53:52 +0700

    update fund transaction

--- api/vbee/src/main/java/com/vinplay/vbee/rmq/report/processor/TopVinhDanhProcessor.java
+++ api/vbee/src/main/java/com/vinplay/vbee/rmq/report/processor/TopVinhDanhProcessor.java
@@ -30,7 +30,7 @@ public class TopVinhDanhProcessor {
                 MediaType mediaType = MediaType.parse("application/json");
                 RequestBody body = RequestBody.create(mediaType, topVinhDanh.toJson());
                 Request request = new Request.Builder()
-                        .url("http://45.76.178.154:8087/leaderboard?boardName")
+                        .url("http://localhost:8087/leaderboard?boardName")
                         .method("POST", body)
                         .addHeader("Content-Type", "application/json")
                         .build();
