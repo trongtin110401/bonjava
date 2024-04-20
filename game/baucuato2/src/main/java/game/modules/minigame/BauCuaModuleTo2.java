4ecba4981bcf2c48620beb431cb178236b569231
oxy <oxyhome6868@gmail.com>
2024-04-20 22:52:45 +0700

    UPDATE BAUCUA

--- game/baucuato2/src/main/java/game/modules/minigame/BauCuaModuleTo2.java
+++ game/baucuato2/src/main/java/game/modules/minigame/BauCuaModuleTo2.java
@@ -209,7 +209,7 @@ public class BauCuaModuleTo2 extends BaseClientRequestHandler {
     public void updateGameStatePerSecond() {
         for (MGRoom entry : this.rooms.values()) {
             MGRoomBauCuaTo2 room = (MGRoomBauCuaTo2) entry;
-            room.botBet(60 - this.count, this.isBettingRound, this);
+            room.botBet(60 - this.count, this.isBettingRound);
         }
     }
 
