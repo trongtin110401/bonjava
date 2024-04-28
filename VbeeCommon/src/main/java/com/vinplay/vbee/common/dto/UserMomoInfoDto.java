124f9f5adcc05e56ada7d4774f4f8b1ea836161c
oxyhome6688 <oxyhome6688@gmail.com>
2024-04-28 22:36:37 +0700

    update user bank and momo

---
+++ VbeeCommon/src/main/java/com/vinplay/vbee/common/dto/UserMomoInfoDto.java
@@ -0,0 +1,54 @@
+/*
+ * Decompiled with CFR 0.144.
+ */
+package com.vinplay.vbee.common.dto;
+
+public class UserMomoInfoDto {
+    private String nickName;
+    private String phoneName;
+    private String phoneNumber;
+    private String createdDate;
+
+    public UserMomoInfoDto(String nickName, String phoneName, String phoneNumber, String createdDate) {
+        this.nickName = nickName;
+        this.phoneName = phoneName;
+        this.phoneNumber = phoneNumber;
+        this.createdDate = createdDate;
+    }
+
+    public UserMomoInfoDto() {
+    }
+
+    public String getPhoneName() {
+        return phoneName;
+    }
+
+    public void setPhoneName(String phoneName) {
+        this.phoneName = phoneName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+}
+
---
+++ api/VinPlayPortal/src/main/java/com/vinplay/api/processors/GetUserMomoInfoProcessor.java
@@ -0,0 +1,27 @@
+package com.vinplay.api.processors;
+
+import com.vinplay.usercore.service.impl.UserServiceImpl;
+import com.vinplay.vbee.common.cp.BaseProcessor;
+import com.vinplay.vbee.common.cp.Param;
+import com.vinplay.vbee.common.dto.UserMomoInfoDto;
+import com.vinplay.vbee.common.response.UserMomoInfoResponse;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+public class GetUserMomoInfoProcessor
+        implements BaseProcessor<HttpServletRequest, String> {
+
+    public String execute(Param<HttpServletRequest> param) {
+        UserMomoInfoResponse response = new UserMomoInfoResponse(false, "1001");
+        HttpServletRequest request = param.get();
+        String nickName = request.getParameter("nickName");
+        UserServiceImpl service = new UserServiceImpl();
+        List<UserMomoInfoDto> momo = service.getListMomoByNickname(nickName);
+        response.setMomo(momo);
+        response.setSuccess(true);
+        response.setErrorCode("0");
+        return response.toJson();
+    }
+}
+
