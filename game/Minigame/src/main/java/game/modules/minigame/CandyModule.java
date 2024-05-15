8e5a9d67c651e633ef3400937ef7f0222e8cbf38
TU TRINH <tu.trinh@weeds.co.kr>
2024-05-15 10:46:07 +0700

    U

--- game/Minigame/src/main/java/game/modules/minigame/CandyModule.java
+++ game/Minigame/src/main/java/game/modules/minigame/CandyModule.java
@@ -211,11 +211,16 @@ public class CandyModule
     }
 
     private void playPokeGo(User user, DataCmd dataCmd) {
+
+        System.out.println("Request Play Candy: " + user.getName());
+
         PlayPokeGoCmd cmd = new PlayPokeGoCmd(dataCmd);
         MGRoomCandy room = (MGRoomCandy) user.getProperty((Object) "MGROOM_" + this.gameName + "_INFO");
         if (room != null) {
             room.play(user, cmd.lines);
         }
+
+        System.out.println("Response Play Candy: " + user.getName());
     }
 
     private void autoPlay(User user, DataCmd dataCMD) {
