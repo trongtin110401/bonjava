eb66f87ebd93690193dfc7abf77947c8d2f1063d
oxy <oxyhome6868@gmail.com>
2024-05-25 23:42:51 +0700

    api for checking counties that are allowed to access hotuppdate

---
+++ api/BoardService/src/main/java/game/controller/HotUpdateController.java
@@ -0,0 +1,31 @@
+package game.controller;
+
+import com.maxmind.geoip2.DatabaseReader;
+import com.maxmind.geoip2.exception.GeoIp2Exception;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+
+@RestController
+@RequestMapping(path = "/hotupdate")
+public class HotUpdateController {
+
+    @GetMapping("/check")
+    public ResponseEntity<String> getByUsernameAndGameName(HttpServletRequest request, HttpServletResponse response) throws IOException, GeoIp2Exception {
+        String ip = request.getRemoteAddr();
+        File database = ResourceUtils.getFile("classpath:GeoLite2-Country.mmdb");
+        DatabaseReader dbReader = new DatabaseReader.Builder(database).build();
+        String country = dbReader.country(InetAddress.getByName(ip)).getCountry().getIsoCode();
+
+        return ResponseEntity.ok(country);
+    }
+
+}
