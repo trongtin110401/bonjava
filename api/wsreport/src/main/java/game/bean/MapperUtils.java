ee3854e06f69e826e8098e20c92508422da430bc
oxy <oxyhome6868@gmail.com>
2024-05-30 00:20:45 +0700

    U

--- api/wsreport/src/main/java/game/bean/MapperUtils.java
+++ api/wsreport/src/main/java/game/bean/MapperUtils.java
@@ -1,6 +1,7 @@
 package game.bean;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class MapperUtils {
@@ -10,5 +11,6 @@ public class MapperUtils {
     static {
         mapper = new ObjectMapper();
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 }
