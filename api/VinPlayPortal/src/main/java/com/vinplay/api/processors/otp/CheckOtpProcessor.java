8f56412b16e71ee62c6f4eec264cab4359c6ef7f
oxyhome6688 <oxyhome6688@gmail.com>
2024-06-02 22:22:50 +0700

    update call back

--- api/VinPlayPortal/src/main/java/com/vinplay/api/processors/otp/CheckOtpProcessor.java
+++ api/VinPlayPortal/src/main/java/com/vinplay/api/processors/otp/CheckOtpProcessor.java
@@ -10,14 +10,21 @@
  */
 package com.vinplay.api.processors.otp;
 
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
 import com.vinplay.usercore.service.impl.OtpServiceImpl;
 import com.vinplay.vbee.common.cp.BaseProcessor;
 import com.vinplay.vbee.common.cp.Param;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.vinplay.vbee.common.mongodb.MongoDBConnectionFactory;
 import com.vinplay.vbee.common.response.BaseResponseModel;
+import com.vinplay.vbee.common.response.UserTele;
+import okhttp3.*;
 import org.apache.log4j.Logger;
+import org.bson.Document;
 
 public class CheckOtpProcessor implements BaseProcessor<HttpServletRequest, String> {
     private static final Logger logger = Logger.getLogger("api");
@@ -30,6 +37,10 @@ public class CheckOtpProcessor implements BaseProcessor<HttpServletRequest, Stri
             OtpServiceImpl service = new OtpServiceImpl();
             try {
                 BaseResponseModel model = service.checkOTP(nickname, otp);
+                if (model.isSuccess()){
+                    UserTele userTele = getInfoByNickname(nickname);
+                    sendMessage(userTele.getChatID(), "Xin chào " + nickname + " b?n ?ã kích ho?t telegram OTP thành công");
+                }
                 return model.toJson();
             } catch (Exception e) {
                 logger.debug(e);
@@ -37,5 +48,72 @@ public class CheckOtpProcessor implements BaseProcessor<HttpServletRequest, Stri
         }
         return "1";
     }
+
+    public static void sendMessage(String chatId, String message) {
+        try {
+//            String bootToken = GameCommon.getValueStr("Telegram_boot_bon_token");
+            String bot = "6831621160:AAHPfkEON1-u2e44F8WAVdu5vT9ySql8ztA";
+            RequestBody requestBody = new FormBody.Builder()
+                    .add("chat_id", chatId)
+                    .add("text", message)
+                    .build();
+
+            Request request = new Request.Builder()
+                    .url("https://api.telegram.org/bot" + bot + "/sendMessage")
+                    .post(requestBody)
+                    .build();
+
+            OkHttpClient client = new OkHttpClient();
+            Response response = client.newCall(request).execute();
+            System.out.println("Response Code: " + response.code());
+            response.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private UserTele getInfoByNickname(String nickname) {
+        MongoDatabase db = MongoDBConnectionFactory.getDB();
+        MongoCollection<Document> collection = db.getCollection("user_tele");
+        Document filter = new Document("nickname", nickname);
+        MongoCursor<Document> cursor = collection.find(filter).iterator();
+
+        try {
+            if (cursor.hasNext()) {
+                Document doc = cursor.next();
+                UserTele user = extractUserInfo(doc);
+                return user;
+            } else {
+                return null;
+            }
+        } finally {
+            cursor.close();
+        }
+
+    }
+
+    private UserTele extractUserInfo(Document doc) {
+        String id = doc.getObjectId("_id").toString();
+        String nickname = doc.getString("nickname");
+        String phoneNumber = doc.getString("phoneNumber");
+        boolean isActive = doc.getBoolean("isActive");
+        String otp = doc.getString("otp");
+        long timeToExpired = doc.getInteger("timeToExpired");
+        String createdDate = doc.getString("createdDate");
+        String chatID = doc.getString("chatID");
+        UserTele user = new UserTele();
+        user.setId(id);
+        user.setNickname(nickname);
+        user.setPhoneNumber(phoneNumber);
+        user.setActive(isActive);
+        user.setOtp(otp);
+        user.setTimeToExpired(timeToExpired);
+        user.setCreatedDate(createdDate);
+        user.setChatID(chatID);
+
+        return user;
+    }
 }
+
+
 
