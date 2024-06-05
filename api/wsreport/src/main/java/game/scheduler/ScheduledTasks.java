80a0eacd0af5df4125094f52147bf283088af3d7
oxyhome6688 <oxyhome6688@gmail.com>
2024-06-05 23:27:54 +0700

    update giftcode

--- api/wsreport/src/main/java/game/scheduler/ScheduledTasks.java
+++ api/wsreport/src/main/java/game/scheduler/ScheduledTasks.java
@@ -151,7 +151,7 @@ public class ScheduledTasks { // chay schedule lien tuc // cach nay chi dung cho
             this.sendMessToAdminBauCua(response.toJson());
 
         } catch (Exception e) {
-            log.error(e.getMessage(), e);
+            e.printStackTrace();
         }
     }
 
