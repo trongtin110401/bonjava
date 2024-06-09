5662e37d7be8db43a9910279e9002e4111b3e48e
oxy <oxyhome6868@gmail.com>
2024-06-10 00:54:28 +0700

    U

--- game/tlmn/src/main/java/game/tienlen/server/TienlenGameServer.java
+++ game/tlmn/src/main/java/game/tienlen/server/TienlenGameServer.java
@@ -1060,7 +1060,6 @@ extends GameServer {
                             GameRoomManager.instance().leaveRoom(gp.getUser());
                         }
                     } else {
-                        System.out.println("==============> user: " + gp.getUser().getName() + " is kicked by pPrepareNewGame");
                         this.removePlayerAtChair(i, false);
                     }
                     msg.hasInfoAtChair[i] = false;
