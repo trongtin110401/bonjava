8324e65773df42d1e2437026c47fa6e694d1b29c
oxy <oxyhome6868@gmail.com>
2024-06-12 13:50:18 +0700

    U

--- game/xocdia/src/main/java/game/xocdia/server/XocDiaGameServer.java
+++ game/xocdia/src/main/java/game/xocdia/server/XocDiaGameServer.java
@@ -408,7 +408,7 @@ public class XocDiaGameServer
 //            cacheService.setValue("XocDia_Flag_Time", String.valueOf(this.getCountTime())); // get all time
             cacheService.setValue("XocDia_Flag_Time", String.valueOf(33 - this.countTime)); // get all time
             cacheService.setValue("XocDia_Flag_GameState", this.gameState); // get all time
-            cacheService.setValue("XocDia_Flag_betting", String.valueOf(this.isBetting));
+            cacheService.setValue("XocDia_Flag_betting", String.valueOf(this.enableBetting));
             cacheService.setValue("XocDia_Flag_session", String.valueOf(this.gameId));
 
             // get when join user or betting
