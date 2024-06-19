ce1fcd0f297ed51d7eac4c0ed51712facc429667
oxy <oxyhome6868@gmail.com>
2024-06-19 22:44:33 +0700

    U

--- game/taixiuMini/src/main/java/game/modules/minigame/room/MGRoomTaiXiu.java
+++ game/taixiuMini/src/main/java/game/modules/minigame/room/MGRoomTaiXiu.java
@@ -269,7 +269,9 @@ public class MGRoomTaiXiu
             }
         }
         inputTime = this.getRemainTime(); // lấy thời gian còn lại
+        UserCacheModel userCacheModel = userService.getMoneyUser(nickname);
         long currentMoney = this.userService.getMoneyUserCache(nickname, this.moneyTypeStr);  // lấy số tiền hiện tại của user dựa trên nick name
+        System.out.println("===========>>>>>>>>> CURRENT MONEY: " + userCacheModel.getNickname() + " = " + userCacheModel.getMoney("vin"));
         int result = 2;
         if (this.enableBetting) {
             if (betValue >= 100L) {
