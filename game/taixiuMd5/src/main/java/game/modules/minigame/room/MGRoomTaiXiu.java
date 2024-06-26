8a9dcee16867dd8ce52f065c3eddcc04b7e2f665
oxy <oxyhome6868@gmail.com>
2024-06-27 00:16:47 +0700

    U

--- game/taixiuMd5/src/main/java/game/modules/minigame/room/MGRoomTaiXiu.java
+++ game/taixiuMd5/src/main/java/game/modules/minigame/room/MGRoomTaiXiu.java
@@ -169,9 +169,8 @@ public class MGRoomTaiXiu extends MGRoom {
 
     // todo : lấy thời gian còn lại bằng cách lấy thời gian hiện tại trừ đi thời gian bắt đầu
     public short getRemainTime() {
-        long currentTime = System.currentTimeMillis();
         if (this.bettingRound) {
-            return (short) (52 - this.module.count);
+            return (short) (50 - this.module.count);
         }
         return (short) (65 - this.module.count);
     }
