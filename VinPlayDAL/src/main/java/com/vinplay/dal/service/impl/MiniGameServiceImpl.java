3d68de8849dbb6241c37abcf6917608b094088ae
oxy <oxyhome6868@gmail.com>
2024-07-01 10:40:24 +0700

    U

--- VinPlayDAL/src/main/java/com/vinplay/dal/service/impl/MiniGameServiceImpl.java
+++ VinPlayDAL/src/main/java/com/vinplay/dal/service/impl/MiniGameServiceImpl.java
@@ -52,6 +52,14 @@ public class MiniGameServiceImpl
 
     @Override
     public void saveFund(String fundName, long value) throws IOException, TimeoutException, InterruptedException {
+
+        System.out.println("Printing stack trace:");
+        StackTraceElement[] elements = Thread.currentThread().getStackTrace();
+        for (int i = 1; i < elements.length; i++) {
+            StackTraceElement s = elements[i];
+            System.out.println("\tat " + s.getClassName() + "." + s.getMethodName() + "(" + s.getFileName() + ":" + s.getLineNumber() + ")");
+        }
+
         UpdateFundMessage msg = new UpdateFundMessage();
         msg.fundName = fundName;
         msg.newValue = value;
