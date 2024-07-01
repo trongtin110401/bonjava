b4080d557627e254f5a7071f2176a75738c472d5
oxy <oxyhome6868@gmail.com>
2024-07-01 12:58:50 +0700

    U

--- game/slot/src/main/java/game/modules/slot/room/SlotRoom.java
+++ game/slot/src/main/java/game/modules/slot/room/SlotRoom.java
@@ -79,9 +79,9 @@ public abstract class SlotRoom {
 
     protected Random random = new Random();
 
-    public SlotRoom(byte id, String gameName, String room, int betValue, short moneyType, long pot, long fun, long initPotValue) {
+    public SlotRoom(byte id, String gamename, String room, int betValue, short moneyType, long pot, long fun, long initPotValue) {
         this.id = id;
-        this.gameName = gameName;
+        this.gameName = gamename;
         this.name = room;
         this.betValue = betValue;
         this.moneyType = moneyType;
