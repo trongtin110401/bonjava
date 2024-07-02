dca56bc2cdd3ee07b0974a16dd3795e1346e1ae2
oxy <oxyhome6868@gmail.com>
2024-07-02 22:43:09 +0700

    U

--- api/VinPlayBackend/src/main/java/com/vinplay/api/backend/processors/taixiu/GetCacheResultTaiXiuMd5Processor.java
+++ api/VinPlayBackend/src/main/java/com/vinplay/api/backend/processors/taixiu/GetCacheResultTaiXiuMd5Processor.java
@@ -5,6 +5,7 @@ import com.vinplay.dal.service.CacheService;
 import com.vinplay.dal.service.impl.CacheServiceImpl;
 import com.vinplay.vbee.common.cp.BaseProcessor;
 import com.vinplay.vbee.common.cp.Param;
+import com.vinplay.vbee.common.enums.Games;
 import org.apache.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
@@ -22,8 +23,7 @@ public class GetCacheResultTaiXiuMd5Processor implements BaseProcessor<HttpServl
             hashMap.put("tai_xiu_be_cang",cacheService.getValueStr("tai_xiu_be_cang_md5"));
             hashMap.put("min_fund_tx_auto",cacheService.getValueStr("min_fund_tx_auto_md5"));
             hashMap.put("max_fund_tx_auto",cacheService.getValueStr("max_fund_tx_auto_md5"));
-            hashMap.put("fund_tx_md5_auto",cacheService.getValueStr("fund_tx_auto_md5"));
-
+            hashMap.put("fund_tx_md5_auto",cacheService.getValueStr(Games.TAI_XIU_MD5.getName()));
 
             hashMap.put("my_debug",cacheService.getValueStr("my_debug_md5"));
 
