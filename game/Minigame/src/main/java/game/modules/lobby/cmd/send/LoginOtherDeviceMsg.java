450541433fcd80bf541200cb6bc44d7dceef2ae0
oxy <oxyhome6868@gmail.com>
2024-07-07 00:43:44 +0700

    U

--- game/Minigame/src/main/java/game/modules/lobby/cmd/send/LoginOtherDeviceMsg.java
+++ game/Minigame/src/main/java/game/modules/lobby/cmd/send/LoginOtherDeviceMsg.java
@@ -8,7 +8,11 @@ import java.nio.ByteBuffer;
 
 public class LoginOtherDeviceMsg extends BaseMsgEx {
 
+    public static final int TYPE_KICK = 1;
+    public static final int TYPE_LOGIN_OTHER_DEVICE = 2;
+
     public String s = "1";
+    public short type = 1;
 
     public LoginOtherDeviceMsg() {
         super(MiniGameCMD.LOGIN_OTHER_DEVICE);
