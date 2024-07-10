c060132350e5fe322b96ca59f4f83f6aafc73067
oxyhome6688 <oxyhome6688@gmail.com>
2024-07-10 21:06:23 +0700

    A

--- VbeeCommon/src/main/java/com/vinplay/vbee/common/utils/VinPlayUtils.java
+++ VbeeCommon/src/main/java/com/vinplay/vbee/common/utils/VinPlayUtils.java
@@ -186,7 +186,7 @@ public class VinPlayUtils {
     }
 
     public static Date getDateTimeFromDate(String date) throws ParseException {
-        SimpleDateFormat formatDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat formatDate = new SimpleDateFormat("yyyy-MM-dd");
         return formatDate.parse(date);
     }
 
