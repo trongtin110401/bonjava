0f7384656c18f46a9c2b33154f9fb3033f4a8f5f
oxy <oxyhome6868@gmail.com>
2024-07-13 09:22:31 +0700

    U

--- api/VinPlayBackend/src/main/java/com/vinplay/api/backend/report/utils/ReportMoneyUtils.java
+++ api/VinPlayBackend/src/main/java/com/vinplay/api/backend/report/utils/ReportMoneyUtils.java
@@ -1,6 +1,6 @@
 /*
  * Decompiled with CFR 0.144.
- * 
+ *
  * Could not load the following classes:
  *  com.hazelcast.core.HazelcastInstance
  *  com.hazelcast.core.IMap
@@ -41,6 +41,7 @@ import com.vinplay.vbee.common.mongodb.MongoDBConnectionFactory;
 import com.vinplay.vbee.common.pools.ConnectionPool;
 import com.vinplay.vbee.common.statics.Consts;
 import com.vinplay.vbee.common.utils.VinPlayUtils;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -54,12 +55,13 @@ import java.util.Map;
 import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
+
 import org.apache.log4j.Logger;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
 public class ReportMoneyUtils {
-    private static final Logger logger = Logger.getLogger((String)"report");
+    private static final Logger logger = Logger.getLogger((String) "report");
 
     public static void init() {
         try {
@@ -68,8 +70,8 @@ public class ReportMoneyUtils {
             HazelcastInstance client = HazelcastClientFactory.getInstance();
             IMap<String, ReportModel> reportMap = client.getMap("cacheReports");
             for (Map.Entry entry : reportMap.entrySet()) {
-                ReportModel model = (ReportModel)entry.getValue();
-                if (model.isBot || !((String)entry.getKey()).contains(today)) continue;
+                ReportModel model = (ReportModel) entry.getValue();
+                if (model.isBot || !((String) entry.getKey()).contains(today)) continue;
                 push = false;
                 break;
             }
@@ -80,9 +82,9 @@ public class ReportMoneyUtils {
                     if (i == 0) {
                         isBot = true;
                     }
-                    Map<String, ReportModel>  maps = dao.getListReportModelByDay(today, isBot);
+                    Map<String, ReportModel> maps = dao.getListReportModelByDay(today, isBot);
                     for (Map.Entry entry2 : maps.entrySet()) {
-                        reportMap.put(entry2.getKey().toString(), (ReportModel)entry2.getValue());
+                        reportMap.put(entry2.getKey().toString(), (ReportModel) entry2.getValue());
                     }
                 }
             }
@@ -100,46 +102,46 @@ public class ReportMoneyUtils {
             cal.set(12, minute);
             cal.set(13, 0);
             Timer timer = new Timer();
-            timer.schedule((TimerTask)new ReportMoneyTask(), cal.getTime(), 30000L);
-            timer.schedule((TimerTask)new AgentTask(), AgentUtils.getFirstDayAfterMonth());
-        }
-        catch (Exception e) {
+
+            timer.schedule((TimerTask) new ReportMoneyTask(), cal.getTime(), 30000L);
+            timer.schedule((TimerTask) new AgentTask(), AgentUtils.getFirstDayAfterMonth());
+        } catch (Exception e) {
             e.printStackTrace();
-            logger.debug((Object)e);
+            logger.debug((Object) e);
         }
     }
 
     public static void fixYesterdayData(String today, final String yesterday) throws ParseException {
-        logger.info((Object)("fixYesterdayData start at: " + new Date()));
-        String timeLog = VinPlayUtils.getDateTimeStr((Date)VinPlayUtils.getDateTimeFromDate((String)yesterday));
+        logger.info((Object) ("fixYesterdayData start at: " + new Date()));
+        String timeLog = VinPlayUtils.getDateTimeStr((Date) VinPlayUtils.getDateTimeFromDate((String) yesterday));
         final HashMap<String, ReportModel> reportMap = new HashMap();
         HashMap<String, ReportModel> reportActionMap = new HashMap<String, ReportModel>();
         MongoDatabase db = MongoDBConnectionFactory.getDB();
         Document conditions = new Document();
         String startTime = timeLog;
-        String endTime = VinPlayUtils.getDateTimeStr((Date)VinPlayUtils.getDateTimeFromDate((String)today));
+        String endTime = VinPlayUtils.getDateTimeStr((Date) VinPlayUtils.getDateTimeFromDate((String) today));
         BasicDBObject obj = new BasicDBObject();
-        obj.put("$gte", (Object)startTime);
-        obj.put("$lt", (Object)endTime);
-        conditions.put("trans_time", (Object)obj);
-        conditions.put("is_bot", (Object)false);
-        FindIterable iterable = db.getCollection("log_money_user_vin").find((Bson)conditions);
-        iterable.forEach((Block)new Block<Document>(){
+        obj.put("$gte", (Object) startTime);
+        obj.put("$lt", (Object) endTime);
+        conditions.put("trans_time", (Object) obj);
+        conditions.put("is_bot", (Object) false);
+        FindIterable iterable = db.getCollection("log_money_user_vin").find((Bson) conditions);
+        iterable.forEach((Block) new Block<Document>() {
 
             public void apply(Document document) {
-                String serviceName = document.getString((Object)"service_name");
+                String serviceName = document.getString((Object) "service_name");
                 if (serviceName != null && !serviceName.equals("Tài xi - Tán l\u1ed9c") && !serviceName.equals("Tài x\u1ec9u - R\u00fat l\u1ed9c")) {
                     String key;
-                    String nickname = document.getString((Object)"nick_name");
-                    String actionname = document.getString((Object)"action_name");
-                    long money = document.getLong((Object)"money_exchange");
-                    long fee = document.getLong((Object)"fee");
-                    Boolean playGame = document.getBoolean((Object)"play_game");
+                    String nickname = document.getString((Object) "nick_name");
+                    String actionname = document.getString((Object) "action_name");
+                    long money = document.getLong((Object) "money_exchange");
+                    long fee = document.getLong((Object) "fee");
+                    Boolean playGame = document.getBoolean((Object) "play_game");
                     if (playGame == null) {
                         playGame = ReportMoneyUtils.checkPlayGame(actionname);
                     }
                     if (reportMap.containsKey(key = nickname + "," + actionname + "," + yesterday)) {
-                        ReportModel reportModel = (ReportModel)reportMap.get(key);
+                        ReportModel reportModel = (ReportModel) reportMap.get(key);
                         if (playGame.booleanValue()) {
                             if (money > 0L) {
                                 ReportModel reportModel2 = reportModel;
@@ -175,24 +177,24 @@ public class ReportMoneyUtils {
         });
         ReportDaoImpl dao = new ReportDaoImpl();
         for (Map.Entry entry : reportMap.entrySet()) {
-            if (((ReportModel)entry.getValue()).isBot) continue;
-            String[] arr = ((String)entry.getKey()).split(",");
+            if (((ReportModel) entry.getValue()).isBot) continue;
+            String[] arr = ((String) entry.getKey()).split(",");
             String nickname = arr[0];
             String actionname = arr[1];
-            dao.saveLogMoneyForReport(nickname, actionname, yesterday, (ReportModel)entry.getValue());
-            ReportModel sumModel = reportActionMap.containsKey(actionname) ? (ReportModel)reportActionMap.get(actionname) : new ReportModel(0L, 0L, 0L, 0L, false);
+            dao.saveLogMoneyForReport(nickname, actionname, yesterday, (ReportModel) entry.getValue());
+            ReportModel sumModel = reportActionMap.containsKey(actionname) ? (ReportModel) reportActionMap.get(actionname) : new ReportModel(0L, 0L, 0L, 0L, false);
             ReportModel reportModel = sumModel;
-            reportModel.moneyWin += ((ReportModel)entry.getValue()).moneyWin;
+            reportModel.moneyWin += ((ReportModel) entry.getValue()).moneyWin;
             ReportModel reportModel2 = sumModel;
-            reportModel2.moneyLost += ((ReportModel)entry.getValue()).moneyLost;
+            reportModel2.moneyLost += ((ReportModel) entry.getValue()).moneyLost;
             ReportModel reportModel3 = sumModel;
-            reportModel3.moneyOther += ((ReportModel)entry.getValue()).moneyOther;
+            reportModel3.moneyOther += ((ReportModel) entry.getValue()).moneyOther;
             ReportModel reportModel4 = sumModel;
-            reportModel4.fee += ((ReportModel)entry.getValue()).fee;
+            reportModel4.fee += ((ReportModel) entry.getValue()).fee;
             reportActionMap.put(actionname, sumModel);
         }
         dao.saveReportMoneyVin(reportActionMap);
-        logger.info((Object)("fixYesterdayData success at: " + new Date()));
+        logger.info((Object) ("fixYesterdayData success at: " + new Date()));
     }
 
     public static boolean checkPlayGame(String actionname) {
@@ -205,14 +207,13 @@ public class ReportMoneyUtils {
 
     public static boolean checkBot(IMap<String, UserCacheModel> userMap, String nickname) {
         boolean isBot = false;
-        if (userMap.containsKey((Object)nickname)) {
-            isBot = ((UserCacheModel)userMap.get((Object)nickname)).isBot();
+        if (userMap.containsKey((Object) nickname)) {
+            isBot = ((UserCacheModel) userMap.get((Object) nickname)).isBot();
         } else {
             try {
                 isBot = ReportMoneyUtils.checkBotDB(nickname);
-            }
-            catch (Exception e) {
-                logger.debug((Object)e);
+            } catch (Exception e) {
+                logger.debug((Object) e);
             }
         }
         return isBot;
