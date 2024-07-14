3d90290b2f51154f9614085d5cd111a0a2fc5454
oxy <oxyhome6868@gmail.com>
2024-07-14 17:59:56 +0700

    u

--- api/VinPlayPortal/src/main/java/com/vinplay/api/utils/PortalUtils.java
+++ api/VinPlayPortal/src/main/java/com/vinplay/api/utils/PortalUtils.java
@@ -89,6 +89,9 @@ public class PortalUtils {
 
         // kick user logging in other device
         String pf = request.getParameter("pf");
+
+        System.out.println("============> User " + userModel.getNickname() + " login with pf: " + pf);
+
         if (pf != null && pf.equalsIgnoreCase("FISH")) {
             // do nothing
         } else {
