62c6aa2f01d3a6f27efcf67fba561afd96fd4330
oxy <oxyhome6868@gmail.com>
2024-07-31 01:25:02 +0700

    U

--- api/VinPlayBackend/src/main/java/com/vinplay/api/backend/processors/GetUserOnlineProcessor.java
+++ api/VinPlayBackend/src/main/java/com/vinplay/api/backend/processors/GetUserOnlineProcessor.java
@@ -1,10 +1,10 @@
 package com.vinplay.api.backend.processors;
 
-import bitzero.server.entities.User;
-import bitzero.util.ExtensionUtility;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
+import com.vinplay.dal.dao.ReportDAO;
 import com.vinplay.dal.dao.impl.LogMoneyUserDaoImpl;
+import com.vinplay.dal.dao.impl.ReportDaoImpl;
 import com.vinplay.vbee.common.cp.BaseProcessor;
 import com.vinplay.vbee.common.cp.Param;
 import com.vinplay.vbee.common.hazelcast.HazelcastClientFactory;
@@ -26,7 +26,6 @@ public class GetUserOnlineProcessor implements BaseProcessor<HttpServletRequest,
 
     @Override
     public String execute(Param<HttpServletRequest> param) {
-
         HttpServletRequest request = param.get();
         UserOnlineResponse response = new UserOnlineResponse(true, "200");
         HazelcastInstance instance = HazelcastClientFactory.getInstance();
@@ -35,13 +34,12 @@ public class GetUserOnlineProcessor implements BaseProcessor<HttpServletRequest,
         int pageIndex = getParameter(request, "pageIndex", 1);
         int pageSize = getParameter(request, "pageSize", 50);
 
-        List<User> users = ExtensionUtility.globalUserManager.getAllUsers();
-
         List<String> usernames = getPage(new ArrayList<>(userOnline.keySet()), pageIndex, pageSize);
         LogMoneyUserDaoImpl dao = new LogMoneyUserDaoImpl();
 
         List<UserCCUResponse> userOnlineResponse = usernames.stream()
                 .map(username -> createUserCCUResponse(username, dao))
+                .sorted((u1, u2) -> Double.compare(u2.getTotalDeposit(), u1.getTotalDeposit()))
                 .collect(Collectors.toList());
 
         response.setUsers(userOnlineResponse);
@@ -51,6 +49,13 @@ public class GetUserOnlineProcessor implements BaseProcessor<HttpServletRequest,
     private UserCCUResponse createUserCCUResponse(String username, LogMoneyUserDaoImpl dao) {
         List<LogUserMoneyResponse> responses = dao.getMoneyCashInAndCashOutByNickname(username, ACTION_NAME);
 
+        ReportDAO reportDAO = new ReportDaoImpl();
+        long currentMoeny = 0;
+        try {
+            currentMoeny = reportDAO.getCurrentMoney(username);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         long totalDeposit = 0;
         long totalCashOut = 0;
 
@@ -66,6 +71,7 @@ public class GetUserOnlineProcessor implements BaseProcessor<HttpServletRequest,
 
         UserCCUResponse userCCUResponse = new UserCCUResponse();
         userCCUResponse.setNickName(username);
+//        userCCUResponse.setTotalMoney(currentMoeny);
         userCCUResponse.setTotalCashOut(totalCashOut);
         userCCUResponse.setTotalDeposit(totalDeposit);
 
@@ -106,3 +112,4 @@ public class GetUserOnlineProcessor implements BaseProcessor<HttpServletRequest,
         return list.subList(fromIndex, toIndex);
     }
 }
+
