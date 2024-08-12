d2e5e0775a99228ec6ecebb244cd730be4e8c0b1
oxy <oxyhome6868@gmail.com>
2024-08-12 23:55:28 +0700

    U

--- game/slot/src/main/java/game/modules/slot/room/Slot25ExtendRoom.java
+++ game/slot/src/main/java/game/modules/slot/room/Slot25ExtendRoom.java
@@ -323,7 +323,7 @@ public class Slot25ExtendRoom extends SlotRoom {
                                     // Tuy không trúng JACKPOT nhưng trúng Line to quá cũng cần sinh lại MATRIX
                                     if (!isGetJackpotNaturally) {
 //                                    if ((totalPrizes - totalBetValue > 0 && totalPrizes > getFunValue()) || totalPrizes >= totalBetValue * 25)
-                                        if (totalPrizes > getFunValue())
+                                        if (totalPrizes > 0 && totalPrizes > getFunValue())
                                             continue;
                                     }
                                 }
