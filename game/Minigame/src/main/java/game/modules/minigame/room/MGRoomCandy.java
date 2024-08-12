55f4254c4ddbb576d88ba0f5435375744c51a41a
oxy <oxyhome6868@gmail.com>
2024-08-12 23:01:46 +0700

    U

--- game/Minigame/src/main/java/game/modules/minigame/room/MGRoomCandy.java
+++ game/Minigame/src/main/java/game/modules/minigame/room/MGRoomCandy.java
@@ -191,6 +191,7 @@ public class MGRoomCandy extends MGRoom {
                         synchronized (this) {
                             block4:
                             while (!enoughPair) {
+                                System.out.println("=================>  lap wishky");
                                 result = 0;
                                 awardsOnLines.clear();
                                 totalPrizes = 0L;
