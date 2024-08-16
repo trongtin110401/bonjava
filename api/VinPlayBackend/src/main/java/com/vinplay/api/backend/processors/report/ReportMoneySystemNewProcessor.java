87ce1417c4aaa161c7d4e0540ad9cd2480131c03
oxy <oxyhome6868@gmail.com>
2024-08-17 01:01:56 +0700

    U

--- api/VinPlayBackend/src/main/java/com/vinplay/api/backend/processors/report/ReportMoneySystemNewProcessor.java
+++ api/VinPlayBackend/src/main/java/com/vinplay/api/backend/processors/report/ReportMoneySystemNewProcessor.java
@@ -209,10 +209,8 @@ public class ReportMoneySystemNewProcessor implements BaseProcessor<HttpServletR
                         || log.actionName.equals(Games.BONG_LAI_CAC.getName())
                         || log.actionName.equals(Games.LIEN_MINH.getName()))
                         && log.getDescription().startsWith("Đặt cược")) {
-//            log.setMoneyExchange(log.moneyExchange - (log.moneyExchange) / 100);
             if (log.moneyExchange < 0) {
                 model.moneyLost += log.moneyExchange;
-                model.setMoneyOther(log.moneyExchange / 100);
             } else {
                 model.moneyWin += log.moneyExchange;
             }
