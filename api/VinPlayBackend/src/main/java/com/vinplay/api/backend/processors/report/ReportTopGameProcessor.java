639767e52420603d7577cf55d0545d2c7ff30bdd
oxyhome6688 <oxyhome6688@gmail.com>
2024-08-18 14:09:58 +0700

    A

--- api/VinPlayBackend/src/main/java/com/vinplay/api/backend/processors/report/ReportTopGameProcessor.java
+++ api/VinPlayBackend/src/main/java/com/vinplay/api/backend/processors/report/ReportTopGameProcessor.java
@@ -236,6 +236,35 @@ public class ReportTopGameProcessor implements BaseProcessor<HttpServletRequest,
                         long money = otherService.getMoneyShootFishByNickname(sqlDateStart, sqlDateEnd, lose.getNickname());
                         lose.setMoneyWin(lose.getMoneyWin() + money);
                     }
+                    List<TopCaoThu> topCaoThus = otherService.getBanCa(sqlDateStart, sqlDateEnd);
+
+                    if (topUserWin.isEmpty()) {
+                        for (TopCaoThu topCaoThu : topCaoThus) {
+                            if (topCaoThu.getMoneyWin() >= 0) {
+                                topUserWin.add(topCaoThu);
+                            }
+                        }
+                    }
+                    if (topUserLost.isEmpty()) {
+                        for (TopCaoThu topCaoThu : topCaoThus) {
+                            if (topCaoThu.getMoneyWin() <= 0) {
+                                topUserLost.add(topCaoThu);
+                            }
+                        }
+                    }
+                    for (TopCaoThu topCaoThu : topCaoThus) {
+                        boolean exists = topUserLost.stream()
+                                .anyMatch(user -> user.getNickname().equals(topCaoThu.getNickname())) ||
+                                topUserWin.stream()
+                                        .anyMatch(user -> user.getNickname().equals(topCaoThu.getNickname()));
+                        if (!exists) {
+                            if (topCaoThu.getMoneyWin() <= 0) {
+                                topUserLost.add(topCaoThu);
+                            } else if (topCaoThu.getMoneyWin() >= 0) {
+                                topUserWin.add(topCaoThu);
+                            }
+                        }
+                    }
                 }
                 if (action.equals("Exchange")) {
                     for (TopCaoThu win : topUserWin) {
