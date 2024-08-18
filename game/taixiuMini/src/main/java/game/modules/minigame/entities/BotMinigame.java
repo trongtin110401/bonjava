ed58e91951bfda402d9088db3787831b790532e0
oxy <oxyhome6868@gmail.com>
2024-08-18 11:51:59 +0700

    U

--- game/taixiuMini/src/main/java/game/modules/minigame/entities/BotMinigame.java
+++ game/taixiuMini/src/main/java/game/modules/minigame/entities/BotMinigame.java
@@ -331,7 +331,7 @@ public class BotMinigame {
                     //random số tiền cho bot trong mảng đã add "betValues"
                     long betValue = betValues.get(n).intValue();
                     short bettingTime = (short) BotMinigame.randomBettingTime(minBettingTime, maxBettingTime, phanTramVaoSom);
-                    if(bettingTime > 55){
+                    if(bettingTime > 49){
                         bettingTime = (short) BotMinigame.randomBettingTime(minBettingTime, 48, phanTramVaoSom);
                     }
                     short betSide = 0;
