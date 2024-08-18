0f2d1b397b4d5ec508f5a02c59fabc4087a8e5e8
oxyhome6688 <oxyhome6688@gmail.com>
2024-08-18 14:34:34 +0700

    A

--- api/VinPlayBackend/src/main/java/com/vinplay/api/backend/processors/report/ReportTopGameProcessor.java
+++ api/VinPlayBackend/src/main/java/com/vinplay/api/backend/processors/report/ReportTopGameProcessor.java
@@ -37,14 +37,7 @@ import com.vinplay.vbee.common.utils.VinPlayUtils;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
+import java.util.*;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
@@ -219,8 +212,6 @@ public class ReportTopGameProcessor implements BaseProcessor<HttpServletRequest,
                     if (++n < num) continue;
                     break;
                 }
-                res.topUserWin = topUserWin;
-                res.topUserLost = topUserLost;
                 OtherService otherService = new OtherServiceImpl();
                 LocalDate dateStart = LocalDate.parse(startTime, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
                 String sqlDateStart = dateStart.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
@@ -307,6 +298,10 @@ public class ReportTopGameProcessor implements BaseProcessor<HttpServletRequest,
 
 
                 }
+                topUserWin.sort((user1, user2) -> Long.compare(user2.getMoneyWin(), user1.getMoneyWin()));
+                topUserLost.sort(Comparator.comparingLong(TopCaoThu::getMoneyWin));
+                res.topUserWin = topUserWin;
+                res.topUserLost = topUserLost;
                 res.topBotWin = topBotWin;
                 res.topBotLost = topBotLost;
                 res.setErrorCode("0");
