98d96c2b0b4c0860531c1aa26564a60dfb453ba8
oxy <oxyhome6868@gmail.com>
2024-08-22 01:39:32 +0700

    U

--- VinPlayUserCore/src/main/java/com/vinplay/usercore/dao/impl/XocDiaDaoImpl.java
+++ VinPlayUserCore/src/main/java/com/vinplay/usercore/dao/impl/XocDiaDaoImpl.java
@@ -256,7 +256,7 @@ implements XocDiaDao {
                 BetResult betResult = new BetResult();
                 transactionXocDia.setUsername(document.getString("user_name"));
                 transactionXocDia.setTotalPrize(document.getLong("total_prize"));
-                transactionXocDia.setTotalExchange(document.getLong("money_exchange") + document.getLong("total_prize"));
+                transactionXocDia.setTotalExchange(document.getLong("money_exchange"));
                 transactionXocDia.setTimestamp(document.getString("create_time"));
                 betResult.setZeroWhite(document.getLong("zero_white"));
                 betResult.setFourWhite(document.getLong("four_white"));
