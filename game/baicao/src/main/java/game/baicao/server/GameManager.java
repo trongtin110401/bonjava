a9fe69d48de11d64b1a24adc9f6cc0698aa1f475
oxy <oxyhome6868@gmail.com>
2024-08-25 15:14:05 +0700

    U

--- game/baicao/src/main/java/game/baicao/server/GameManager.java
+++ game/baicao/src/main/java/game/baicao/server/GameManager.java
@@ -59,7 +59,7 @@ public class GameManager {
     }
 
     public void gameLoop() {
-                gameServer.log("===> gameState + " + gameState + " - count down: " + countDown);
+        gameServer.log("===> gameState + " + gameState + " - game action: " + gameAction + " - count down: " + countDown);
         if (this.gameState == GS_NO_START && this.isAutoStart) {
             --this.countDown;
             if (this.countDown <= 0) {
