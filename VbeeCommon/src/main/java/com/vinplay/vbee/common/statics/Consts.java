4de6f11bbb82c65766f17fa30a64baa332a2d9a5
oxy <oxyhome6868@gmail.com>
2024-09-03 15:05:52 +0700

    U

--- VbeeCommon/src/main/java/com/vinplay/vbee/common/statics/Consts.java
+++ VbeeCommon/src/main/java/com/vinplay/vbee/common/statics/Consts.java
@@ -207,7 +207,7 @@ public class Consts {
             "KhoBau", "NuDiepVien", "SieuAnhHung", "VuongQuocVin", "Sam", "BaCay", "Binh", "Tlmn", "TaLa", "Lieng", "XiTo",
             "BaiCao", "Poker", "PokerTour", "XocDia", "XiDzach", "Caro", "CoTuong", "CoVua", "CoUp", "HamCaMap", CANDY, AUDITION, MAYBACH, BENLEY, RANGE_ROVER, Spartan, TAMHUNG, ROLL_ROYE,
             AUDITION_FREE, MAYBACH_FREE, BENLEY_FREE, RANGE_ROVER_FREE, Spartan_FREE, TAMHUNG_FREE, TAMHUNG_FREE, ROLL_ROYE_FREE, Games.COWBOY.getName(), Games.FAST_AND_FURIOUS.getName(),
-            Games.LADY_NIGHT.getName(), Games.CARIBE.getName(), Games.BONG_LAI_CAC.getName(), Games.HALLOWEEN.getName(), Games.LAS_VEGAS.getName(),
+            Games.LADY_NIGHT.getName(), Games.BIG_CITY_BOY.getName(), Games.BONG_LAI_CAC.getName(), Games.HALLOWEEN.getName(), Games.LAS_VEGAS.getName(),
             Games.SEXY_DANCE.getName(), Games.LIEN_MINH.getName(), Games.TAI_XIU_MD5.getName(), "BauCuaTo"
     );
 
