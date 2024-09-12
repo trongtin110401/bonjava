730eba0f9e0bb556e92d154a7a2ee71b68260fbd
oxyhome6688 <oxyhome6688@gmail.com>
2024-09-12 23:45:11 +0700

    fix get phone active

--- api/VinPlayPortal/src/main/java/com/vinplay/api/processors/ActivePhoneProcessor.java
+++ api/VinPlayPortal/src/main/java/com/vinplay/api/processors/ActivePhoneProcessor.java
@@ -80,6 +80,7 @@ public class ActivePhoneProcessor implements BaseProcessor<HttpServletRequest, S
 //            response.setSuccess(false);
 //            response.setErrorCode("Số điện thoại không hợp lệ");
 //        }
+        response.setErrorCode("200");
         response.setSuccess(true);
         return response.toJson();
     }
