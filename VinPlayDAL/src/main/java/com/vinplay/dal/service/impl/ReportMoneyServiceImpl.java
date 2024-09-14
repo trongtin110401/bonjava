cc9d09c1dcfc50770d3b0c858d494ddc4f5dc1ea
oxy <oxyhome6868@gmail.com>
2024-09-14 18:53:55 +0700

    U

--- VinPlayDAL/src/main/java/com/vinplay/dal/service/impl/ReportMoneyServiceImpl.java
+++ VinPlayDAL/src/main/java/com/vinplay/dal/service/impl/ReportMoneyServiceImpl.java
@@ -49,12 +49,12 @@ public class ReportMoneyServiceImpl implements ReportMoneyService {
         AggregateIterable<Document> aggregateResult =  collection.aggregate(Arrays.asList(
                 match(and(filters)),
                 group("$action_name"
-                        ,sum("$action_name", "$action_name")
                         ,sum("total_out", "$total_out")
                         ,sum("total_refund", "$total_refund")
                         ,sum("total_in", "$total_in")
                         ,sum("fee", "$fee")
-                        ,sum("revenue", "$revenue")
+                        ,sum("revenue", "$revenue"),
+                        first("action_name", "$action_name")
                 )  // GROUP BY và SUM
 
         ));
