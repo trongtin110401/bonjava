a47221d1d9469a09f8aca95d54aa3d29b0b3d509
oxy <oxyhome6868@gmail.com>
2024-09-14 18:10:38 +0700

    U

--- api/vbee/src/main/java/com/vinplay/vbee/rmq/log/processor/LogMoneyUserProcessor.java
+++ api/vbee/src/main/java/com/vinplay/vbee/rmq/log/processor/LogMoneyUserProcessor.java
@@ -87,6 +87,7 @@ public class LogMoneyUserProcessor implements BaseProcessor<byte[], Boolean> {
         map.put("nick_name", report.getNickName());
         map.put("action_name", report.getActionName());
         map.put("report_date", report.getReportDate());
+        map.put("s_game", report.isGame);
 
 
         // Define the query filter
@@ -98,9 +99,7 @@ public class LogMoneyUserProcessor implements BaseProcessor<byte[], Boolean> {
                         .append("total_refund", report.totalRefund)
                         .append("total_in", report.totalIn)
                         .append("fee", report.getFee())
-                        .append("revenue", report.getRevenue())
-                        .append("is_game", report.isGame())
-        );
+                        .append("revenue", report.getRevenue()));
 
         // Define the options (upsert: true)
         UpdateOptions options = new UpdateOptions().upsert(true);
