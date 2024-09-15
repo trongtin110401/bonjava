9c8ba46167de584c628d002cc8794eb3841fad1b
oxy <oxyhome6868@gmail.com>
2024-09-15 19:14:44 +0700

    u

--- VinPlayUserCore/src/main/java/com/vinplay/usercore/service/impl/GiftCodeServiceImpl.java
+++ VinPlayUserCore/src/main/java/com/vinplay/usercore/service/impl/GiftCodeServiceImpl.java
@@ -469,7 +469,7 @@ public class GiftCodeServiceImpl
         // Tạo pipeline của aggregation
         MongoCollection<Document> col = db.getCollection("gift_code");
         AggregateIterable<Document> result = col.aggregate(Arrays.asList(
-                Document.parse("[\n" +
+                Document.parse(
                         "  {\n" +
                         "    $group: {\n" +
                         "      _id: \"$type\",\n" +
@@ -489,8 +489,7 @@ public class GiftCodeServiceImpl
                         "        }\n" +
                         "      }\n" +
                         "    }\n" +
-                        "  }\n" +
-                        "]")
+                        "  }\n" )
         ));
 
         // Gán lại kết quả thống kê cho campaign
