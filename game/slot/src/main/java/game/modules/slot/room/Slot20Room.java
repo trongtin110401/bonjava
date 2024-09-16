5bd4eadd88401dec4676b1188b68d736127d3b21
oxy <oxyhome6868@gmail.com>
2024-09-17 00:49:25 +0700

    u

--- game/slot/src/main/java/game/modules/slot/room/Slot20Room.java
+++ game/slot/src/main/java/game/modules/slot/room/Slot20Room.java
@@ -90,7 +90,7 @@ public class Slot20Room extends SlotRoom {
         }
     }
 
-    public SLot20ResultMsg play(String username, String linesStr) {
+    public synchronized SLot20ResultMsg play(String username, String linesStr) {
 
         // kết quả mặc định
         short result = ResultSlot.MISSED;
